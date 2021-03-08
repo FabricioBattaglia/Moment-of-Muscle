03ec9cfaf10cfb6f905e0c72ef7a41df28188c7c
FabricioBattaglia <55457491+FabricioBattaglia@users.noreply.github.com>
2021-03-07 19:39:46 -0500

    Update Email Verification

--- src/EmailVerification.java
+++ src/EmailVerification.java
@@ -1,4 +1,4 @@
-package com.example.momentofmuscle;
+package com.example.mom;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -49,33 +49,9 @@ public class EmailVerification extends AppCompatActivity {
         goToHome.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                //STILL HAS TO BE WORKED ON (CHECK IF USER IS VERIFIED)
-                //FirebaseAuth checkAuth = FirebaseAuth.getInstance();
-                //FirebaseUser checkUser = checkAuth.getCurrentUser();
-                //if(checkUser.isEmailVerified()) {
-                //   goToHomeVerified();
-                //}
-                //else{
-                //    Toast.makeText(EmailVerification.this, "Your profile is not verified, please verify your e-mail", Toast.LENGTH_SHORT).show();
-                //}
-                //tried sending back to register screen since it checks if the user
-                //is verified or not, but it worked the same as the above code
-                goToHomeVerified();
-            }
-        });
-
-        Button logoutButton = (Button) findViewById(R.id.eLogout);
-        logoutButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
                 logout1();
             }
         });
-    }
-
-    public void goToHomeVerified(){
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
     }
     public void logout1() {
         FirebaseAuth.getInstance().signOut();//logout
